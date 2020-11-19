adeea6f206152c6324b2080bced8de634d2c16bd
mottoteen <superware@qq.com>
2020-11-19 22:29:13 +0800

    最长回文子串

---
+++ 5.longest-palindromic-substring.cpp
@@ -0,0 +1,67 @@
+/*
+
+给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+
+示例 1：
+
+	输入: "babad"
+	
+	输出: "bab"
+	
+	注意: "aba" 也是一个有效答案。
+
+示例 2：
+
+	输入: "cbbd"
+	
+	输出: "bb"
+
+*/
+class Solution {
+public:
+    int expand(const string& s, int left, int right) {
+        while (left >= 0 && right < s.size() && s[left] == s[right]) {
+            --left;
+            ++right;
+        }
+        return (right - left - 2) / 2;
+    }
+
+    string longestPalindrome(string s) {
+        int start = 0, end = -1;
+        string t = "#";
+        for (char c: s) {
+            t += c;
+            t += '#';
+        }
+        vector<int> arm_len;
+        int right = -1, j = -1;
+        for (int i = 0; i < t.size(); ++i) {
+            int cur_arm_len;
+            if (right >= i) {
+                int i_sym = j * 2 - i;
+                int min_arm_len = min(arm_len[i_sym], right - i);
+                cur_arm_len = expand(t, i - min_arm_len, i + min_arm_len);
+            } else {
+                cur_arm_len = expand(t, i, i);
+            }
+            arm_len.push_back(cur_arm_len);
+            if (i + cur_arm_len > right) {
+                j = i;
+                right = i + cur_arm_len;
+            }
+            if (cur_arm_len * 2 + 1 > end - start) {
+                start = i - cur_arm_len;
+                end = i + cur_arm_len;
+            }
+        }
+
+        string ans;
+        for (int i = start; i <= end; ++i) {
+            if (t[i] != '#') {
+                ans += t[i];
+            }
+        }
+        return ans;
+    }
+};
