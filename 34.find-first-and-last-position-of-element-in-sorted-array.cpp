c317ec14caaee72c1540f8df9392af93efe9e90c
mottoteen <superware@qq.com>
2020-12-01 23:58:15 +0800

    在排序数组中查找元素的第一个和最后一个位置

---
+++ 34.find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -0,0 +1,62 @@
+/*
+
+给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+
+如果数组中不存在目标值 target，返回 [-1, -1]。
+
+进阶：
+
+	你可以设计并实现时间复杂度为 O(log n) 的算法解决此问题吗？
+
+示例 1：
+
+	输入：nums = [5,7,7,8,8,10], target = 8
+
+	输出：[3,4]
+
+示例 2：
+
+	输入：nums = [5,7,7,8,8,10], target = 6
+
+	输出：[-1,-1]
+
+示例 3：
+
+	输入：nums = [], target = 0
+	
+	输出：[-1,-1]
+
+提示：
+
+	0 <= nums.length <= 10^5
+	
+	-10^9 <= nums[i] <= 10^9
+	
+	nums 是一个非递减数组
+	
+	-10^9 <= target <= 10^9
+
+*/
+class Solution { 
+public:
+    int binarySearch(vector<int>& nums, int target, bool lower) {
+        int left = 0, right = (int)nums.size() - 1, ret = (int)nums.size();
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > target || (lower && nums[mid] == target)) {
+                right = mid - 1;
+                ret = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ret;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int left = binarySearch(nums, target, true);
+        int right = binarySearch(nums, target, false) - 1;
+        if (left <= right && right < nums.size() && nums[left] == target && nums[right] == target)
+            return vector<int>{left, right};
+        return vector<int>{-1, -1};
+    }
+};
